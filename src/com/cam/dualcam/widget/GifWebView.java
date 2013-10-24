0786c9be681290b974119c81a0672614f2e880a9
rj <jpactstaff4@gmail.com>
2013-10-24 16:23:14 +0800

    White side at GifWebView fixed

--- src/com/cam/dualcam/widget/GifWebView.java
+++ src/com/cam/dualcam/widget/GifWebView.java
@@ -21,7 +21,7 @@ public class GifWebView extends WebView {
 		String baseUrl = pPath.substring(0, pPath.lastIndexOf("/") + 1);
 		String fileName = pPath.substring(pPath.lastIndexOf("/")+1);
 		StringBuilder strBuilder = new StringBuilder();
-		strBuilder.append("<html><head><style type='text/css'>body{margin:auto auto;text-align:center;} img{width:100%25;} </style>");
+		strBuilder.append("<html><head><style type='text/css'>body{margin:auto auto;text-align:center;background-color: #ffffff;} img{width:100%25;} </style>");
 		strBuilder.append("</head><body>");
 		strBuilder.append("<img src=\"" + fileName + "\" height=\"100%\" width=\"100%\" /></body></html>");
 		String data = strBuilder.toString();
