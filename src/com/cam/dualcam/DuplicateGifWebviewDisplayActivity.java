900cd94e760da4c2472d65570aa661ac8b3fd618
rj <jpactstaff4@gmail.com>
2013-10-31 11:55:20 +0800

    RJ black ops  - Commencing mega change :D

---
+++ src/com/cam/dualcam/DuplicateGifWebviewDisplayActivity.java
@@ -0,0 +1,73 @@
+package com.cam.dualcam;
+
+import com.cam.dualcam.widget.GifWebView;
+import com.cam.dualcam.utility.Field;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DuplicateGifWebviewDisplayActivity extends Activity {
+	
+	private GifWebView gifView;
+	private int orientationOfPhone;
+	private boolean showSpalshScreen = true;
+	private String enterCamera = "カメラ";
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gif_webview_layout);
+		gifView = (GifWebView) findViewById(R.id.gif_view);
+		
+		orientationOfPhone = this.getResources().getConfiguration().orientation;
+		
+		/*gifView.getSettings().setBuiltInZoomControls(true);
+		gifView.getSettings().setSupportZoom(true);
+		gifView.getSettings().setLoadWithOverviewMode(true);*/
+		//gifView.getSettings().setUseWideViewPort(true);
+        
+		if (orientationOfPhone == Configuration.ORIENTATION_PORTRAIT) {
+			gifView.setGifAssetPath("file:///android_asset/falls.gif");
+			gifView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+			//gifView.setGifAssetPath("file:///android_asset/cute.gif");
+			//gifView.setGifAssetPath("file:///android_asset/funny.gif");
+		} else if (orientationOfPhone == Configuration.ORIENTATION_LANDSCAPE) {
+			gifView.setGifAssetPath("file:///android_asset/landscape_train.gif");
+			gifView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+			//gifView.setGifAssetPath("file:///android_asset/karate.gif");
+		} 
+		
+		/*
+		 * 
+		 * gif1.setMovieResource(R.drawable.cute);
+		} else if (orientationOfPhone == Configuration.ORIENTATION_LANDSCAPE) {
+
+			//gif1.setMovieResource(R.drawable.landscape_train);
+			gif1.setMovieResource(R.drawable.karate);
+		} 
+		 */
+
+	//get extras 
+	Bundle extras = getIntent().getExtras();
+	showSpalshScreen = extras.getBoolean("showSplashScreen");
+	
+	Button buttonOne = (Button) findViewById(R.id.btnEnterCamera);
+	buttonOne.setText(enterCamera);
+	buttonOne.setTextSize(36);
+	//buttonOne.setBackgroundColor(Color.TRANSPARENT);
+	buttonOne.setOnClickListener(new Button.OnClickListener() {
+	    public void onClick(View v) {
+	    	finish();
+	    	Intent i = new Intent(DuplicateGifWebviewDisplayActivity.this, DualCamActivity.class); 
+	    	i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			i.putExtra("showSplashScreen", false);
+			startActivity(i);
+	    }
+	});
+		
+	}
+}
